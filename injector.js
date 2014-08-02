58bae55651196063138792b235989bf7f3b10bc4
Develra <michael.aaron@colorado.edu>
2014-08-02 13:02:02 -0400

    helps to try with actual data

--- injector.js
+++ injector.js
@@ -2,8 +2,8 @@ function inject(element){
 	switch (element){
 		case "contact":
 			$.get('./pages/contact.html', function(data) {
-				alert(JSON.stringify(data));
-				return $.parseHTML();
+				alert($.parseHTML(data));
+				return $.parseHTML(data);
 			});
 			//$('#content_body').load("./pages/contact.html");
 			break;
