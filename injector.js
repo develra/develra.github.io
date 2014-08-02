47e7297bd43e2cbfa9eec61c4127f51c154b69c1
Develra <michael.aaron@colorado.edu>
2014-08-02 13:18:45 -0400

    let's see if javascript scope works like I think it should

--- injector.js
+++ injector.js
@@ -3,7 +3,8 @@ function inject(element){
 	switch (element){
 		case "contact":
 			html2inject = $.get('./pages/contact.html', function(data) {
-				return data;
+				html2inject = data;
+				return;
 			});
 			//$('#content_body').load("./pages/contact.html");
 			break;
