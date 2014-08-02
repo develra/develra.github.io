9eb27ae8b4a7ea4011c66f6780f6c6d55b407c6d
Develra <michael.aaron@colorado.edu>
2014-08-02 12:37:08 -0400

    trying with the get method instead

--- injector.js
+++ injector.js
@@ -2,8 +2,11 @@ function inject(element){
 	file = ""
 	switch (element){
 		case "contact":
+			$.get('./pages/contact.html', function(data) {
+				alert( "Data Loaded: " + data);
+			});
 			//$('#content_body').load("./pages/contact.html");
 			break;
 	}
-	return $('#temp').load("./pages/contact.html");
+	return "";
 }
