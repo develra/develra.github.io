c31df80e3d5238d32b7631442371a918aeb02a35
Develra <michael.aaron@colorado.edu>
2014-08-02 12:42:38 -0400

    something is going strange, trying this way out

--- injector.js
+++ injector.js
@@ -1,13 +1,11 @@
 function inject(element){
-	html2inject = ""
 	switch (element){
 		case "contact":
 			$.get('./pages/contact.html', function(data) {
-				html2inject = data;
+				alert(data);
+				return data;
 			});
 			//$('#content_body').load("./pages/contact.html");
 			break;
 	}
-	alert(html2inject);
-	return html2inject.toString();
 }
